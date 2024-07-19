1139a0227b66504f3224889cf096b283c664421b
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-20 00:14:28 +0530

    added-input

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,39 +1,37 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-const Register = () => {
+import RegisterForm from '@/components/forms/RegisterForm'
+
+const Register = ({ user }) => {
   return (
     <div className="flex h-screen max-h-screen">
-    {/* verifiotp */}
-
-  <section className="remo-scrollbar container my-auto">
+      <section className="remo-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-         <Image
-          src="/assets/icons/logo-full.svg"
-          height={1000}
-          width={1000}
-          alt="patient"
-          className="mb-12 h-10 w-fit"
-         />
-
-         {/* <PatientForm/> */}
-         <div className="text-14-regular m-20 flex justify-between">
-         <p className="justify-items-end text-dark-600 xl:text-left"> © 2024 CareFull</p>
-         <Link href="/>admin-true" className="text-green-500">
-         Admin
-         </Link>
-         </div>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <RegisterForm user={user} />
+          <div className="text-14-regular m-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CareFull</p>
+            <Link href="/admin-true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
-  </section>
-
-  <Image 
-  src="/assets/images/register-img.png"
-  height={1000}
-  width={1000}
-  alt="patient"
-  className="side-img max-w-[390px]"
-  />
-</div>
+      </section>
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+    </div>
   )
 }
 
