cb47a9069b03cfd12444cfe5de34b134d9bdc794
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-07-19 23:52:27 +0530

    added-basicpage

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 const Register = () => {
   return (
-    <div className='flex h-screen max-h-screen'>
+    <div className="flex h-screen max-h-screen">
+    {/* verifiotp */}
 
-        <section className='remove-scorllbar container'>
-          <div className='sub-container max-w-[860px] flex-1 flex-col py-10 '>
-            <Image
-            src="/assets/icons/logo.svg"
-            height={1000}
-            width={1000}
-            alt='careful'
-            className='mb-12 h-10 w-full'
-            />
-          </div>
-        </section>
-    </div>
+  <section className="remo-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+         <Image
+          src="/assets/icons/logo-full.svg"
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="mb-12 h-10 w-fit"
+         />
+
+         {/* <PatientForm/> */}
+         <div className="text-14-regular m-20 flex justify-between">
+         <p className="justify-items-end text-dark-600 xl:text-left"> © 2024 CareFull</p>
+         <Link href="/>admin-true" className="text-green-500">
+         Admin
+         </Link>
+         </div>
+        </div>
+  </section>
+
+  <Image 
+  src="/assets/images/register-img.png"
+  height={1000}
+  width={1000}
+  alt="patient"
+  className="side-img max-w-[390px]"
+  />
+</div>
   )
 }
 
